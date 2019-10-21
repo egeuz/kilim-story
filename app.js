1c46e815bd8104032f8168eb97d1353f6cb9195d
Ege Uz <enkomaun@gmail.com>
2019-10-21 17:22:21 -0400

    live site bugfix

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const button = document.getElementById('button');
 let images = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 
 for(let i=0; i<3; i++) {
-  const rng = Math.floor(Math.random() * images.length + 1);
-  container.innerHTML += `<img src=../images/${rng}.png>`;
+  const rng = Math.floor(Math.random() * images.length);
+  container.innerHTML += `<img src=images/${images[rng]}.png alt="motif ${images[rng]}">`;
   images.filter(image => images.indexOf(image) === rng);
 }
